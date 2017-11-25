a12250271484ee8350733dca06dd6fe178047650
Jinxuan Cui <i@cjxol.com>
2017-11-25 15:09:18 +0000

    Done text analysis

---
+++ Core/Core/src/TextDocuments.java
@@ -0,0 +1,47 @@
+import java.util.*;
+
+// Request body
+class TextDocuments {
+    public List<TextDocument> documents;
+
+    public TextDocuments() {
+        this.documents = new ArrayList<>();
+    }
+    public void add(String id, String language, String text) {
+        this.documents.add (new TextDocument(id, language, text));
+    }
+}
+
+class TextDocument {
+    public String id, language, text;
+
+    public TextDocument(String id, String language, String text){
+        this.id = id;
+        this.language = language;
+        this.text = text;
+    }
+}
+
+// Response
+class TextResultDocuments {
+
+    public List<TextResultDocument> documents;
+
+    public TextResultDocuments() {
+        this.documents = new ArrayList<>();
+    }
+    public void add(String[] keyPhrases, String id) {
+        this.documents.add (new TextResultDocument(keyPhrases, id));
+    }
+
+}
+
+class TextResultDocument {
+    public String[] keyPhrases;
+    public String id;
+
+    public TextResultDocument(String[] keyPhrases, String id){
+        this.keyPhrases = keyPhrases;
+        this.id = id;
+    }
+}

f0edcb03c8987bb5a8d8d78766799f43387da810
Jinxuan Cui <i@cjxol.com>
2017-11-25 14:31:20 +0000

    Get MS Cognitive Text API working

---
+++ Core/Core/src/Documents.java
@@ -0,0 +1,21 @@
+import java.util.*;
+public class Documents {
+    public List<Document> documents;
+
+    public Documents() {
+        this.documents = new ArrayList<Document>();
+    }
+    public void add(String id, String language, String text) {
+        this.documents.add (new Document (id, language, text));
+    }
+}
+
+class Document {
+    public String id, language, text;
+
+    public Document(String id, String language, String text){
+        this.id = id;
+        this.language = language;
+        this.text = text;
+    }
+}

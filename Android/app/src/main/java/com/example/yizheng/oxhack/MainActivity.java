e4867bd6dc1efb775c453b9e116c1e2ca7d07218
Yi <zhengyi20201@gmail.com>
2017-11-26 05:48:14 +0000

    Fix tab bar on the top

--- Android/app/src/main/java/com/example/yizheng/oxhack/MainActivity.java
+++ Android/app/src/main/java/com/example/yizheng/oxhack/MainActivity.java
@@ -8,6 +8,7 @@ import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
+import android.widget.TableLayout;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -56,6 +57,8 @@ public class MainActivity extends AppCompatActivity {
         bindListeners();
 
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+
+
         //navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
 
     }
